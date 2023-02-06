e4ecf70459114395738a6e6e40dd3b037456830a
pedro.darde <pedro.darde@multi24h.com.br>
2023-02-06 07:51:45 -0300

    fix: deixando .env pronto

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -21,4 +21,5 @@ if [ "$role" = "app" ]; then
     php artisan route:clear
     php artisan serve --port="$PORT" --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint "$@"
+    php artisan migrate:fresh --seed
 fi
